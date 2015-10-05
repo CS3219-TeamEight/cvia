e6032aacdbd8b944123f26223e925a01ec09ea0f
hyungjon <hyungjon.kim@gmail.com>
2015-10-05 18:06:37 +0800

    Initialized skeleton for some content analyzer

---
+++ src/CVAnalyzer.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class CVAnalyzer {
+    
+    private String content;
+    private ArrayList<String> sections = new ArrayList<>();
+
+    public CVAnalyzer(String content) {
+        this.content = content;
+    }
+    
+    public void divideToSections() {
+        
+    }
+    
+    public void analyzeWorkExperience() {
+        
+    }
+    
+    public void analyzeEducation() {
+        
+    }
+    
+    public void gradeCV() {
+        
+    }
+
+}
