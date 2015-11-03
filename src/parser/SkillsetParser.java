1d0fa0202f41d369ff476aaf3918e712550e49df
stanley92 <Stanley92@hotmail.com>
2015-11-04 00:27:27 +0800

    Implemented Skillset Parser

---
+++ src/parser/SkillsetParser.java
@@ -0,0 +1,46 @@
+package parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import job.JobDesc;
+
+public class SkillsetParser {
+
+	JobDesc job;
+	
+	private ArrayList<String> skillsets = new ArrayList<String>();
+	private File resume;
+	
+	public SkillsetParser() {
+		
+	}
+	
+	public void findSkillsets (File resume) {
+		this.resume = resume;
+		determineSkillsets();
+	}
+
+	private void determineSkillsets() {
+		try {
+			FileReader fr = new FileReader(resume);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null) {
+            	for (String skill : job.getskillSets()) {
+            		if (Pattern.compile(skill).matcher(line).find())
+            			skillsets.add(skill);
+            	}
+            }
+            fr.close();
+            br.close();
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+}
