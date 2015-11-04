2ce22b5114976f698e5f66e56b72afde129cd276
Jerrold Law <jerroldlaw1@gmail.com>
2015-11-04 19:45:55 +0800

    Added example job desc for testing

--- src/main/ExampleJob.java
+++ src/main/ExampleJob.java
@@ -6,6 +6,16 @@ public class ExampleJob {
 	JobDesc exampleDesc;
 	
 	public ExampleJob(){
-		exampleDesc.set
+		exampleDesc.setEduWeightage("2");
+		exampleDesc.setWorkWeightage("3");
+		exampleDesc.setLanguageWeightage("2");
+		exampleDesc.setSkillsetWeightage("5");
+		
+		exampleDesc.workExp("2 years");
+		exampleDesc.setLanguages("english, chinese");
+		exampleDesc.setSkillSets("java, python, ruby, ajax");
+		exampleDesc.setJobTitle("Software Engineer");
+		exampleDesc.setEducationLevel("2");
+		exampleDesc.setEducationTitle("Computer Science");
 	}
 }
