a45a00002e8f3bf6c21f70b4f3a9d790a549c9b5
stanley92 <Stanley92@hotmail.com>
2015-11-05 15:49:51 +0800

    Modified Scorer for workExp

--- src/scorer/Scorer.java
+++ src/scorer/Scorer.java
@@ -1,46 +1,45 @@
 package scorer;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 import job.JobDesc;
-import parser.LanguageParser;
 import main.ParseResultStorage;
-import qualification.Education;
 import qualification.WorkExp;
-import main.ParseResultStorage;
 
 public class Scorer {
-	
+
 	ParseResultStorage result;
 	JobDesc jobDesc;
-	
+
 	public Scorer(JobDesc jobDesc, ParseResultStorage result){
 		this.jobDesc = jobDesc;
 		this.result = result;
 	}
-	
+
 	private double computeWorkScore() {
-	    double duration = 0;
-	    double workExpScore = 0;
-	    double jobTitleScore = 0;
-	    
-	    if(jobDesc.getWorkDuration() != 0) {
-		    for (WorkExp work : result.getWorkExp()) {
-		        duration += work.getDuration();
-		    }
-		    workExpScore = (duration / jobDesc.getWorkDuration()) * jobDesc.getWorkWeightage() / 10;
-		    
-		//	    for (WorkExp work : result.getWorkExp()) {
-		//	    	if()
-		//	    }
-		    
-		    return workExpScore + jobTitleScore;
-	    } else {
-	    	return jobDesc.getWorkWeightage();
-	    }
+		double duration = 0;
+		double workExpScore = 0;
+		int jobCount = 0;
+
+		for (WorkExp work : result.getWorkExp()) {
+			if (work.getTitle().equalsIgnoreCase(jobDesc.getJobTitle())) {
+				duration += work.getDuration();
+				jobCount ++;
+			}
+		}
+		
+		if (jobCount > 0)
+			workExpScore = duration / jobDesc.getWorkDuration();
+		else 
+			workExpScore = 0;
+		
+		if(jobDesc.getWorkDuration() != 0) { 
+			return workExpScore * jobDesc.getWorkWeightage() / 10;
+		} else {
+			return jobDesc.getWorkWeightage() / 10;
+		}
 	}
-	
+
 	public double computeScore(){
 		double score = 0;
 		System.out.println("Resume Scores");
@@ -50,33 +49,33 @@ public class Scorer {
 		score += computeLanguageScore();
 		System.out.println("Skills Score: " + computeSkillsScore());
 		score += computeSkillsScore();
-		
-		
+
+
 		return score;
 	}
-	
+
 	private double computeWorkExpScore(JobDesc jobDesc, double workExp, double expectedExp){
 		return workExp / expectedExp * jobDesc.getWorkWeightage() / 10;
 	}
-	
+
 	private double computeSkillsScore() {
 		double score= 0;
 		int numOfSkills = 0;
 		ArrayList<String> skills = result.getSkills();
-		
+
 		// Loop through to find the number of languages that matches the Job Description
 		for (int i = 0; i < skills.size(); i++) {
 			if (jobDesc.getskillSets().contains(skills.get(i).toLowerCase())){
 				numOfSkills ++;
 			}
 		}
-		
+
 		double base = jobDesc.getskillSets().size();
 		score = numOfSkills/base;
-		
+
 		return score * (double) jobDesc.getSkillsetWeightage() / 10;
 	}
-	
+
 	private double computeLanguageScore() {
 		double score = 0;
 		int numOfLanguage = 0;
@@ -88,10 +87,10 @@ public class Scorer {
 				numOfLanguage++;
 			}
 		}
-		
+
 		double base = jobDesc.getLanguages().size();
 		score = numOfLanguage/base;
-		
+
 		return score * (double) jobDesc.getLanguageWeightage() / 10;
 	}
 }
